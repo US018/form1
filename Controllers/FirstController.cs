002f0f7def1628af81c9cc3109633d41c06bfd08
Muhammad Umar <mu967172@gmail.com>
2022-12-20 21:10:36 +0500

    Add project files.

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,72 @@
+﻿using database.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace database.Controllers
+{
+    public class FirstController : Controller
+    {
+        public readonly FirstDBContaxt _Db;
+        public FirstController(FirstDBContaxt Db)
+        {
+            _Db = Db;
+        }
+        public IActionResult Index()
+        {
+            var datalist = _Db.DataModels.ToList();
+            return View(datalist);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(data modle)
+        {
+            _Db.DataModels.Add(modle);
+            _Db.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _Db.DataModels.Where(x => x.Stdid == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit (data modle)
+        {
+            var data = _Db.DataModels.Where(x => x.Stdid == modle.Stdid).FirstOrDefault();
+            if(data != null)
+            {
+                data.Stdid = modle.Stdid;
+                data.Name = modle.Name;
+                data.Email = modle.Email;
+                data.Age = modle.Age;
+                _Db.SaveChanges();
+
+            }
+            return View();
+        }
+        public ActionResult Details (int id)
+        {
+            var data = _Db.DataModels.Where(x => x.Stdid == id).FirstOrDefault();
+            return View(data);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var ddata = _Db.DataModels.Where(x => x.Stdid == id).FirstOrDefault();
+            _Db.DataModels.Remove(ddata);
+            _Db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Models/data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace database.Models
+{
+    public class data
+    {
+        [Key]
+        public int Stdid { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        public string Name { get; set; }
+        [Column(TypeName ="varchar(20)")]
+
+        public string Email { get; set; }
+
+        public DateTime Age { get; set; }
+
+
+
+    }
+}
